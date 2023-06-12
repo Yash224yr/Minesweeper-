6cc0154da8c9435ad8d55a0c90d83caf3d868d95
yash <yash224.yr@gmail.com>
2023-06-12 16:13:11 +0530

    commit

---
+++ src/index.js
@@ -0,0 +1,56 @@
+import React, { Profiler } from 'react';
+import ReactDOM from 'react-dom/client';
+import Mineweeper from './Mineweeper/Mineweeper';
+// import Button from './Button';
+// import Card from './CardGame/Card';
+// import Memory from './CardGame/Memory';
+// import App from './Ecommerce/App';
+// import Quiz from './Quiz/Quiz';
+// import Home from './MobCompare/Home';
+// import Routing from './ContextApi/Routing';
+// import Movies from './MoviesWeb/Movies';
+// import Routing from './Routing/routing';
+// import Cart from './Cart/cart';
+// import Axios from './Axios/Axios';
+// import {Todo} from "./To-do/Todo"
+// import Tesla from "./Tesla/Tesla"
+// import Tabs from "./button/tabs"
+// import App from './Redux-Counter/App';
+// import Store from './Redux-Counter/Store';
+// import App from './To-Do-Redux/App';
+// import Store from './To-Do-Redux/Store'
+// import { Provider } from 'react-redux';
+// import YT from './YT Api/YT';
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+
+root.render(
+   // <Todo/>                                   
+   // <Tesla/>
+   // <Axios/>
+   // <Tabs/>
+   // <Cart/>
+   // <Routing/>
+   // <Movies/>
+   // <Routing/>
+   // <Home/>
+   // <Quiz/>
+   // <Provider store={Store}>
+   //    {/* <App /> */}
+   //    <App/>
+   // </Provider>
+   // <App></App>
+   // <Memory/>
+   // <Button></Button>
+   // <YT></YT>
+   <Mineweeper></Mineweeper>
+);
+
+
+
+
